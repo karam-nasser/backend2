4c1f677d0d66b893886d46d2d77b49c2fc557fa1
karam-nasser <92330836+karam-nasser@users.noreply.github.com>
2021-11-02 17:29:06 -0500

    Add files via upload

---
+++ back-end-2/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+const {
+getHouses,
+createHouse,
+updateHouse,
+deleteHouse
+} = reqiure('./contoller')
+
+app.get(`/api/houses`, getHouses)
+app.delete('/api/houses/:id', deleteHouse)
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+
+app.listen(4004, () => console.log(`running on 4004`))
